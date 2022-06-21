0913ccb1b1247560ee2789e2f5716fc7122ede4f
Jarek <101186879+jarek167714@users.noreply.github.com>
2022-06-21 10:44:35 +0000

    Napisz program, który obliczy sumę cyfr w liczbie całkowitej.

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    
-  }
+  public static void main(String[] args) 
+  {
+    Scanner nowy = new Scanner(System.in);
+    int x, wynik;
+    System.out.println("Podaj liczbę:  ");
+    x = nowy.nextInt(); 
+    wynik = 0;
+    while (x != 0) 
+    {
+      wynik += x % 10;
+      x /= 10;
+    }
+System.out.println("Suma cyfr w liczbie całkowitej wynosi " + wynik);
+}
 }
